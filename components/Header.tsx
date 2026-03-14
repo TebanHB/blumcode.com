2c1cf72833cb3b6aad603a27ef01b1cb6061595f
Jeffer <antelo123@hotmail.com>
2026-03-14 00:16:44 -0400

    CambioName

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="flex justify-between items-center h-16">
 
           <div className="text-2xl font-bold text-blum-blue">
-            BlumCode
+            Blum
           </div>
 
           <nav className="hidden md:flex space-x-8">
